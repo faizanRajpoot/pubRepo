00e079ba112b160ae05e4f0f446deb5506342e3f
talhaanwar629@gmail.com <talhaanwar629@gmail.com>
2022-11-28 10:39:36 +0500

    Talha

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,7 @@
 export default function Header(){
     return(
-        <div className="w-full h-[450px] bg-red-400" ></div>
+        <div className="w-full h-[450px] " id="header" >
+ 
+        </div>
     )
 }
